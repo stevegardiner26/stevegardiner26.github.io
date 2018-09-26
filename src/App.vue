fd1e15c5bd2a3b38bf29dac93eb47a9f0e5dbb48
Steve Gardiner <stevegardiner26@aol.com>
2018-09-26 12:55:56 -0400

    Vue Js Test Push

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <app-navbar></app-navbar>
+    <component :is="currentPage"></component>
+  </div>
+</template>
+
+<script>
+  import Nav from './components/Navbar.vue';
+  import Home from './components/Home.vue';
+
+  export default {
+    data () {
+      return {
+        currentPage: 'app-home',
+      }
+    },
+    components: {
+      appNavbar: Nav,
+      appHome: Home,
+    },
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container-fluid">
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="#" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">About</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link" @click="changePage('app-contact')">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+
+        }
+      },
+      name: "Navbar",
+      methods: {
+        changePage(nav) {
+          console.log(nav);
+        },
+      },
+    }
+</script>
+
+<style scoped>
+  .container-fluid {
+    padding: 0;
+  }
+
+</style>
