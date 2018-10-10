df40b281e1ed8dfeb52539cc797b2ae1c2b2a60a
Steve Gardiner <stevegardiner26@aol.com>
2018-10-10 18:58:33 -0400

    Added some skills

--- js/skills.js
+++ js/skills.js
@@ -5,16 +5,16 @@ $(".clickcard").click(function(){
     if($(this)[0].classList[1] !== 'flipped') {
         setTimeout(function(){
             parent.style.boxShadow = "1px 1px 5px rgba(0,0,0,.3)";
-            var progress = parent.children[0].children[1].children[0].children[0].children[0];
-            var button = parent.children[0].children[1].children[0].children[6];
+            var progress = parent.children[0].children[1].children[1].children[0].children[0];
+            var button = parent.children[0].children[1].children[1].children[6];
             button.style.opacity = 1;
             progress.style.width = progress.getAttribute('aria-valuenow') + '%';
         },500);
     }
     else {
         setTimeout(function(){
-            var progress = parent.children[0].children[1].children[0].children[0].children[0];
-            var button = parent.children[0].children[1].children[0].children[6];
+            var progress = parent.children[0].children[1].children[1].children[0].children[0];
+            var button = parent.children[0].children[1].children[1].children[6];
             button.style.opacity = 0;
             progress.style.width = 0 + '%';
         },500);
