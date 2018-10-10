cb1d3688e9b0657a54e972abb61548c652545cfc
Steve Gardiner <stevegardiner26@aol.com>
2018-10-10 11:00:56 -0400

    Successful Skill Card Flip

--- js/skills.js
+++ js/skills.js
@@ -1,3 +1,8 @@
 $(".clickcard").click(function(){
+    var parent = $(this).parent()[0];
     $(this).toggleClass("flipped");
+    parent.style.boxShadow = "none";
+    setTimeout(function(){
+        parent.style.boxShadow = "1px 1px 5px rgba(0,0,0,.3)";
+    },500);
 });
