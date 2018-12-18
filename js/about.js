dc23c24af1c5d718b47a818194e18b398a567e18
Steve Gardiner <stevegardiner26@aol.com>
2018-12-18 17:20:08 -0500

    Added Download Button and Finished About Page

--- js/about.js
+++ js/about.js
@@ -1,3 +1,12 @@
 $('.right')[0].onclick = () => {
     location.href = 'projects.html'
 };
+
+$('.fa-instagram')[0].onclick = () => {
+    window.open('http://instagram.com/motivationalsteve');
+};
+
+$('.fa-linkedin')[0].onclick = () => {
+    window.open('https://www.linkedin.com/in/steven-gardiner/');
+};
+
